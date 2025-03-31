8bee680e8d35ff2e39162d115d30a09eac65e9c4
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2025-03-31 09:23:00 -0300

    Add files via upload

--- Aula 04 - Prática Encapsulamento/escola/Aluno.java
+++ Aula 04 - Prática Encapsulamento/escola/Aluno.java
@@ -1,44 +1,132 @@
 package escola;
+
+import java.util.Calendar;
+
 public class Aluno {
+
     private String codigo;
     private String nome;
     private int anoNasc;
     private int mesNasc;
+    private int diaNasc;
     private double nota1;
     private double nota2;
-    
+
     // Construtor
-    public Aluno(String codigo, String nome, int anoNasc, int mesNasc, double nota1, double nota2) {
+    public Aluno(String codigo, String nome, int anoNasc, int mesNasc, int diaNasc, double nota1, double nota2) {
         this.codigo = codigo;
         this.nome = nome;
         this.anoNasc = anoNasc;
         this.mesNasc = mesNasc;
+        this.diaNasc = diaNasc;
         this.nota1 = nota1;
         this.nota2 = nota2;
     }
+
     // anos de vida
     public int calcAnos() {
-        return 2025 - this.anoNasc;
+        Calendar hoje = Calendar.getInstance();
+        int anoAtual = hoje.get(Calendar.YEAR);
+        int mesAtual = hoje.get(Calendar.MONTH) + 1; // Janeiro é 0
+        int diaAtual = hoje.get(Calendar.DAY_OF_MONTH);
+        int idadeAno;
+        if (mesNasc > mesAtual || (mesNasc == mesAtual && diaNasc > diaAtual)) {
+            idadeAno = anoAtual - anoNasc - 1;
+        } else {
+            idadeAno = anoAtual - anoNasc;
+        }
+        return idadeAno;
     }
-    // meses de vida (idade*12)
-    public int calcMeses() {
-        return calcAnos() * 12;
+
+    // meses de vida 
+    public double calcMeses() {
+        Calendar hoje = Calendar.getInstance();
+        int anoAtual = hoje.get(Calendar.YEAR);
+        int mesAtual = hoje.get(Calendar.MONTH) + 1; // Janeiro é 0
+        int diaAtual = hoje.get(Calendar.DAY_OF_MONTH);
+        int anosDiferenca = anoAtual - anoNasc;
+        int mesesDiferenca = mesAtual - mesNasc;
+        int idadeMeses = anosDiferenca * 12 + mesesDiferenca;
+        if (diaAtual < diaNasc) {
+            idadeMeses--; // Se o dia atual é antes do dia de nascimento, subtrai um mês
+        }
+        return idadeMeses;
     }
+
     // media
     public double calcMedia(double nota1, double nota2) {
         return (nota1 + nota2) / 2;
     }
+
     public void escrever() {
         System.out.println(
-        "Codigo: " + codigo + 
-        "\nNome: " + nome + 
-        "\nAno de nascimento: " + anoNasc +
-        "\nMes de nascimento: " + mesNasc + 
-        "\nNota 1: " + nota1 + 
-        "\nNota 2: " + nota2 +
-        "\nAnos de vida: " + calcAnos() + 
-        "\nMeses de vida: " + calcMeses() +
-        "\nMedia: " + calcMedia(nota1, nota2) + "\n");
+                "Codigo: " + codigo
+                + "\nNome: " + nome
+                + "\nAno de nascimento: " + anoNasc
+                + "\nMes de nascimento: " + mesNasc
+                + "\nDia de nascimento: " + diaNasc
+                + "\nNota 1: " + nota1
+                + "\nNota 2: " + nota2
+                + "\nAnos de vida: " + calcAnos()
+                + "\nMeses de vida: " + calcMeses()
+                + "\nMedia: " + calcMedia(nota1, nota2) + "\n");
+    }
+
+    // acessores
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getAno() {
+        return this.anoNasc;
+    }
+
+    public int getMeses() {
+        return this.mesNasc;
+    }
+
+    public int getDias() {
+        return this.diaNasc;
+    }
+
+    public double getNota1() {
+        return this.nota1;
+    }
+
+    public double getNota2() {
+        return this.nota2;
+    }
+
+    // mutantes
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoNasc(int anoNasc) {
+        this.anoNasc = anoNasc;
+    }
+
+    public void setMesNasc(int mesNasc) {
+        this.mesNasc = mesNasc;
+    }
+
+    public void setDiaNasc(int diaNasc) {
+        this.diaNasc = diaNasc;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
     }
 }
-
--- Aula 04 - Prática Encapsulamento/escola/Escola.java
+++ Aula 04 - Prática Encapsulamento/escola/Escola.java
@@ -1,13 +1,13 @@
 package escola;
+
 public class Escola {
     public static void main(String[] args) {
-        Aluno Matheus = new Aluno("001", "Matheus", 2008, 4, 10.0, 9.7);
-        Aluno Nicolas = new Aluno("002", "Nicolas", 2001, 9, 4.0, 2.3);
-        Aluno Phelipe = new Aluno("003", "Phelipe", 2008, 2, 5.0, 3.8);
+        Aluno Matheus = new Aluno("001", "Matheus", 2008, 4, 28, 10.0, 9.7);
+        Aluno Nicolas = new Aluno("002", "Nicolas", 2001, 9, 11, 4.0, 2.3);
+        Aluno Phelipe = new Aluno("003", "Phelipe", 2015, 6, 22, 5.0, 3.8);
         
         Matheus.escrever();
         Nicolas.escrever();
         Phelipe.escrever();
-    }
-    
+    }  
 }
